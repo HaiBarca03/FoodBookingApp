2ec820fc5dffbacc44459cb2bb4bc39cab5a3de5
HaiBarca03 <doanduchai8d2m@gmail.com>
2024-08-07 23:58:26 +0700

    ver 1

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 const placeOrder = async (req, res) => {
 
-    const frontend_url = 'http://localhost:5173'
+    const frontend_url = 'http://localhost:5173/'
 
     try {
         const newOrder = new orderModel({
